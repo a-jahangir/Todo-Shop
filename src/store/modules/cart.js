19e51a0170fc411461e9a4aee2d4908d3e480603
amir hossein jahangir <a.jahangir74@gmail.com>
2023-01-10 20:26:19 +0330

    Local Storage Added

--- src/store/modules/cart.js
+++ src/store/modules/cart.js
@@ -1,23 +1,41 @@
+import Swal from 'sweetalert2'
+
+function updateLocalStorage (cart) {
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
+
 const cart = {
   namespaced: true,
 
   state: {
-    cart: []
+    cart: localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
   },
 
   mutations: {
     add (state, product) {
-      state.cart.push({
-        ...product,
-        quantity: 1
-      })
-      console.log(state.cart)
+      const item = state.cart.find(p => p.id === product.id)
+      if (!item) {
+        state.cart.push({
+          ...product,
+          quantity: 1
+        })
+      } else {
+        item.quantity++
+      }
+      updateLocalStorage(state.cart)
     }
   },
 
   actions: {
     addToCart ({ commit }, product) {
       commit('add', product)
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Product Added',
+        showConfirmButton: false,
+        timer: 1500
+      })
     }
   }
 
